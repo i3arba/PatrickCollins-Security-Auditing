ab03c519ad5c603caf4cc5c5294f6f722f078536
Barba <patrickamc@protonmail.com>
2024-02-16 21:44:32 -0300

    Tincho Challenge from Cyfrin Course

---
+++ tincho-challenge/test/RegistryTest.t.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Registry} from "../src/Registry.sol";
+
+contract RegistryTest is Test {
+    Registry registry;
+    address alice;
+
+    function setUp() public {
+        alice = makeAddr("alice");
+        
+        registry = new Registry();
+    }
+
+    function test_register() public {
+        uint256 amountToPay = registry.PRICE();
+        
+        vm.deal(alice, amountToPay);
+        vm.startPrank(alice);
+
+        uint256 aliceBalanceBefore = address(alice).balance;
+
+        registry.register{value: amountToPay}();
+
+        uint256 aliceBalanceAfter = address(alice).balance;
+        
+        assertTrue(registry.isRegistered(alice), "Did not register user");
+        assertEq(address(registry).balance, registry.PRICE(), "Unexpected registry balance");
+        assertEq(aliceBalanceAfter, aliceBalanceBefore - registry.PRICE(), "Unexpected user balance");
+    }
+    
+    function testStatelessFuzzCatchesTheBug(uint256 randomValue) public {
+        //tincho used vm.assume, and not the conditional as I did.
+        if(randomValue < registry.PRICE()) {
+            return;
+        }
+
+        vm.deal(alice, randomValue);
+        vm.startPrank(alice);
+        registry.register{value: randomValue}();
+
+        assertEq(address(registry).balance, registry.PRICE(), "Unexpected registry balance");
+    }
+}
